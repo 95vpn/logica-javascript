60e32db667213fe447c53cca1a769f07470c89b9
Johnny Herrera <jmjaramillo95@gmail.com>
2025-08-27 23:58:22 -0500

    logica

--- codigo-en-vivo.js
+++ codigo-en-vivo.js
@@ -3,16 +3,34 @@ let palabra = "reconocer";
 let esPalindromo = false;
 let arrayPalabra = palabra.toLowerCase().split("");
 
-for (let i = 0; i < arrayPalabra.length; i++) {
+function palabrasIguales(arrayPalabra)
+{
+    for (let i = 0; i < arrayPalabra.length; i++) {
     for (let j = arrayPalabra.length; j > 0; j--) {
         if(palabra[i] == palabra[j])
         {
             esPalindromo = true;
-        }
-
-        
+        }   
     }
-    
+    return esPalindromo;
 }
 
-console.log(esPalindromo);
+}
+console.log(palabrasIguales("reconocer"))
+
+
+let arreglo = ["hola", "adios", "sol", "luz", "estrella"];
+let obj = {};
+
+for (const element of arreglo) {
+    console.log(element.length)
+    const lengthWord = element.length
+    if(obj[lengthWord] != 0)
+    {
+        obj[lengthWord] = lengthWord;
+        console.log(obj[lengthWord])
+    }
+    // obj[lengthWord].push(element)
+    console.log(obj[lengthWord])
+
+}
